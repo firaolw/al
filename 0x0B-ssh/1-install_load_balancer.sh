2348a39333877a234bb03e24b0a3adbf5d4d4aa5
firaolw <fiasrat125@gmail.com>
2022-10-29 01:30:26 +0000

    upto

--- 0x0B-ssh/1-install_load_balancer.sh
+++ 0x0B-ssh/1-install_load_balancer.sh
@@ -6,6 +6,6 @@ sudo apt-get -y update
 sudo apt-get -y install haproxy
 
 sudo sed -i -e '$aENABLED=1\n' /etc/default/haproxy
-sudo sed -i -e '$a\listen http-in\n\tbind :80\n\tbalance roundrobin\n\tserver 1653-web-01 35.231.216.93:80 check\n\tserver 1653-web-02 35.196.0.213:80 check\n' /etc/haproxy/haproxy.cfg
+sudo sed -i -e '$a\listen http-in\n\tbind :80\n\tbalance roundrobin\n\tserver 40161-web-01 54.166.175.55:80 check\n\tserver 40161-web-02 100.25.192.7:80 check\n' /etc/haproxy/haproxy.cfg
 sudo service haproxy restart
 
